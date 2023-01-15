869491679be68fb337a2b713861cf89e3034f240
OK10101 <kostovynskyioleh@gmail.com>
2023-01-15 22:06:04 +0100

    Refine SpreadsheetStreamer

--- app/models/concerns/spreadsheet_streamer.rb
+++ app/models/concerns/spreadsheet_streamer.rb
@@ -10,13 +10,42 @@ class SpreadsheetStreamer
 
   def stream
     helpdesk_proxy.tickets.each do |ticket|
-      puts ticket.subject
+
+      internal_ticket = Ticket.where(external_id: ticket.id).first_or_create
+
+      if ticket.comments.size <= 1
+        puts "Skipping ticket #{ticket.id} because it doesn't have replies"
+        next
+      end
+
+      if internal_ticket.imported
+        puts "Skipping ticket #{ticket.id} because it is already imported"
+        next
+      end
+
+      result = spreadsheet_manager.insert_rows prepare_row(ticket)
+      
+      if result
+        internal_ticket.update(imported: true) 
+      end
+
+      result
     end
 
     true
   end
 
   private
+
+  def prepare_row(ticket)
+    comments = ticket.comments
+    customer_question = comments[0].body
+    agent_response    = comments[1].body
+
+    ## ROW FORMAT
+    ## Customer Q, Reply, Best reply, Score, Ticket#, Date, Customer email, Agent
+    [[customer_question, agent_response, '', 'TBA', ticket.id, ticket.created_at, ticket.via.dig('source', 'from', 'address'), ticket.assignee_id]]
+  end
 
   def spreadsheet_manager
     @spreadsheet_manager ||= SpreadsheetManager.new(spreadsheet_key, spreadsheet_provider: spreadsheet_provider)

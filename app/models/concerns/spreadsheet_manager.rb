f79ae7db91f652ca8720c47f2b559524ebd6618e
OK10101 <kostovynskyioleh@gmail.com>
2023-01-15 22:51:12 +0100

    Update Spreadsheet Manager

--- app/models/concerns/spreadsheet_manager.rb
+++ app/models/concerns/spreadsheet_manager.rb
@@ -1,36 +1,52 @@
 class SpreadsheetManager
 
-  attr_reader :spreadsheet_key, :spreadsheet_provider
+  attr_reader :spreadsheet, :helpdesk_system, :spreadsheet_provider
 
-  def initialize(spreadsheet_key, spreadsheet_provider: :google)
-    @spreadsheet_key      = spreadsheet_key
-    @spreadsheet_provider = spreadsheet_provider
+  def initialize(spreadsheet, helpdesk_system: :zendesk)
+    @spreadsheet      = spreadsheet
+    @helpdesk_system  = helpdesk_system
   end
 
-  ## Row is a multidimensional array, e.g.
-  ## [["This", "is", "one", "row"],["And", "another"]]
-  def insert_rows(rows, worksheet_title=nil)
-    worksheet = worksheet_title.present? ? worksheets : worksheets.first
+  def stream_tickets
+    helpdesk_proxy.tickets.each do |ticket|
 
-    worksheet.insert_rows(worksheet.num_rows + 1, rows)
-    worksheet.save
+      internal_ticket = Ticket.where(external_id: ticket.id).first_or_create
+
+      if ticket.comments.size <= 1
+        puts "Skipping ticket #{ticket.id} because it doesn't have replies"
+        next
+      end
+
+      if internal_ticket.imported
+        puts "Skipping ticket #{ticket.id} because it is already imported"
+        next
+      end
+
+      result = spreadsheet.insert_rows prepare_row(ticket)
+      
+      if result
+        internal_ticket.update(imported: true) 
+      end
+
+      result
+    end
+
+    true
   end
 
-  def worksheets
-    spreadsheet.worksheets
-  end
-
-  def worksheet_titles
-    worksheets.map(&:title)
-  end
-  
   private
 
-  def spreadsheet
-    @spreadsheet ||= proxy.spreadsheet_by_key(spreadsheet_key)
+  def prepare_row(ticket)
+    comments = ticket.comments
+    customer_question = comments[0].body
+    agent_response    = comments[1].body
+
+    ## ROW FORMAT
+    ## Customer Q, Reply, Best reply, Score, Ticket#, Date, Customer email, Agent
+    [[customer_question, agent_response, '', 'TBA', ticket.id, ticket.created_at, ticket.via.dig('source', 'from', 'address'), ticket.assignee_id]]
   end
 
-  def proxy
-    @proxy ||= "SpreadsheetProxy::#{spreadsheet_provider.to_s.camelcase}SpreadsheetProxy".constantize.new
+  def helpdesk_proxy
+    @helpdesk_proxy ||= "HelpdeskProxy::#{helpdesk_system.to_s.camelcase}Proxy".constantize.new
   end
 end
